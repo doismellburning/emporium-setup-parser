699e282a2056fbb51193436eb9c6bda412978279
Kristian Glass <git@doismellburning.co.uk>
2018-12-20 15:07:31 +0000

    Primitive exec hackery to dump setup arguments
    *No* attempt at even the crudest "sandboxing"...

--- web.py
+++ web.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+import setuptools
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/parse', methods=["POST"])
+def parse():
+    setuppy = request.get_data()
+    setup_data = {}
+
+    def fake_setup(*args, **kwargs):
+        setup_data["install_requires"] = kwargs.get("install_requires")
+        setup_data["extras_require"] = kwargs.get("extras_require")
+        setup_data["tests_require"] = kwargs.get("tests_require")
+
+    setuptools.setup = fake_setup
+
+    exec(setuppy)
+
+    return str(setup_data)
+
 
 if __name__ == '__main__':
     app.run()
